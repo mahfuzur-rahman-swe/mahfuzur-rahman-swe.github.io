bb684a4b56bbc926e4dcee9f1de6dbfc35c96e62
mahfuz <mahfuzur.rahman.swe@gmail.com>
2025-02-05 17:15:55 +0600

    resume created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AboutComponent} from "./about/about.component";
 import {PortfolioComponent} from "./portfolio/portfolio.component";
+import {ResumeComponent} from "./resume/resume.component";
 
 const routes: Routes = [
   {path: '', component: AboutComponent},
   {path: 'portfolio', component: PortfolioComponent},
+  {path: 'resume', component: ResumeComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
+import {MatExpansionModule} from "@angular/material/expansion";
+import { ResumeComponent } from './resume/resume.component';
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
-    PortfolioComponent
+    PortfolioComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
