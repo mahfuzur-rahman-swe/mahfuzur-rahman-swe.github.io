5b0463dfb71b26816022ccbde6945cf5bbaa1e7b
mahfuz <mahfuzur.rahman.swe@gmail.com>
2025-02-11 13:59:56 +0600

    update refresh 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(
+    private router: Router
+  ) {
+  }
+
+  ngOnInit(): void {
+    const urlParams = new URLSearchParams(window.location.search);
+    const redirect = urlParams.get('redirect');
+
+    if (redirect) {
+      this.router.navigateByUrl(redirect);
+    }
+  }
   title = 'my-portfolio';
 }
